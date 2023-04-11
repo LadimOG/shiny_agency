4022b4404379529ebb56db1af44cd188ac40f093
ladimOg <ladim1703@gmail.com>
2023-04-11 12:29:06 +0200

    "avancement cours"

--- src/pages/Freelance.jsx
+++ src/pages/Freelance.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react"
 import Card from "../components/Cards/index"
 import styled from "styled-components"
 import Loader from "../components/Loader"
 import Error from "../components/Error"
+import { useFetch } from "../utils/hooks"
 
 const ContainaierFreelance = styled.div`
   display: flex;
@@ -10,49 +10,40 @@ const ContainaierFreelance = styled.div`
   justify-content: center;
   align-items: center;
   gap: 50px;
+  padding-bottom: 50px;
 `
 const ContainerCard = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 24px;
+  margin: 20px;
 `
 function Freelance() {
-  const [profiles, setProfiles] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const { data, isLoading, error } = useFetch(
+    "http://localhost:8000/freelances"
+  )
 
-  useEffect(() => {
-    async function fetchProfil() {
-      setIsLoading(true)
-      try {
-        const response = await fetch("http://localhost:8000/freelances")
-        const {freelancersList} = await response.json()
-        setProfiles(freelancersList)
-      } catch (error) {
-        console.log(error);
-        setError(true)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    fetchProfil()
-  }, [])
+  const { freelancersList } = data
 
-
-  return  error ? <Error /> :(
-    <ContainaierFreelance >
-      <h1>👩🏻‍💻Freelances👨🏼‍💻</h1>
-      {isLoading ? <Loader /> :(
-      <ContainerCard>
-        {profiles.map((profil, index) => (
-          <Card
-            key={`${profil.name}-${index}`}
-            label={profil.job}
-            picture={profil.picture}
-            title={profil.name}
-          />
-        ))}
-      </ContainerCard>
+  return error ? (
+    <Error />
+  ) : (
+    <ContainaierFreelance>
+      <h1>👩🏻‍💻FREELANCES👨🏼‍💻</h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ContainerCard>
+          {freelancersList &&
+            freelancersList.map((profil, index) => (
+              <Card
+                key={`${profil.name}-${index}`}
+                label={profil.job}
+                picture={profil.picture}
+                title={profil.name}
+              />
+            ))}
+        </ContainerCard>
       )}
     </ContainaierFreelance>
   )
